fb8d45c4ae791a325471488af1c4fcf0efe36897
ydvsahil03 <ydvsahil0003@gmail.com>
2024-04-26 11:31:45 +0530

    first commit

--- bloodbank/urls.py
+++ bloodbank/urls.py
@@ -15,12 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include, RedirectView , staticfiles_storage
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('patient.urls')),
     path('donor/', include('donor.urls')),
     path('bloodbank/', include('bank.urls')),
-     path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('favicon/favicon.ico')))
+  
 ]
