02a9ae56fcf47bc7a49e934b6a74944bfa763e28
ydvsahil03 <ydvsahil0003@gmail.com>
2024-04-26 12:13:23 +0530

    first commit

--- bloodbank/settings.py
+++ bloodbank/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-k*@j6=^91^f^p9ij0=b_)ao(jn*@n7sx8&%bf7q2e4b8d8+w(t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django03.azurewebsites.net','127.0.0.1']
 
 
 # Application definition
