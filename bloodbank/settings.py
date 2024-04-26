fa5031ed9acdd2a2cc9b9312b5042213362d104b
ydvsahil03 <ydvsahil0003@gmail.com>
2024-04-26 09:37:22 +0530

    first commit

--- bloodbank/settings.py
+++ bloodbank/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-k*@j6=^91^f^p9ij0=b_)ao(jn*@n7sx8&%bf7q2e4b8d8+w(t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app','.now.sh']
 
 
 # Application definition
